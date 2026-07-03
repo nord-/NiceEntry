nord-/NiceEntry

commit 3eb2653d71dc2fd3e13f35dfab8f043b0509fdb7

refactor: clean up control binding mechanisms and event lifecycle
Editable properties (Text, Date, Time, ItemsSource, SelectedIndex, SelectedItem) were both two-way bound via SetBinding and mirrored through a propertyChanged handler, so a value set from the inner control round-tripped back as a redundant write. Drop the redundant handlers and rely on the two-way binding. (#8)

Make ItemDisplayBinding a BindableProperty instead of a plain CLR property that forwarded straight to the inner Picker, removing the null-forgiving getter. (#5)

Attach the Entry.Focused handler on Loaded and detach it on Unloaded so the inner control doesn't retain the subscription across recycling. (#9)

Closes #8

Closes #5

Closes #9

NiceEntry/LabeledPicker.xaml.cs
@@ -18,12 +18,13 @@ public partial class LabeledPicker
         UpdateTitleColorView();
     }
     
-    public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(nameof(ItemsSource), typeof(IList), typeof(LabeledPicker), propertyChanged: ItemSourceChanged, defaultBindingMode: BindingMode.TwoWay);
-    public static readonly BindableProperty SelectedIndexProperty = BindableProperty.Create(nameof(SelectedIndex), typeof(int), typeof(LabeledPicker), -1, propertyChanged: SelectedIndexChanged, defaultBindingMode: BindingMode.TwoWay);
-    public static readonly BindableProperty SelectedItemProperty = BindableProperty.Create(nameof(SelectedItem), typeof(object), typeof(LabeledPicker), propertyChanged: SelectedItemChanged, defaultBindingMode: BindingMode.TwoWay);
+    public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(nameof(ItemsSource), typeof(IList), typeof(LabeledPicker), defaultBindingMode: BindingMode.TwoWay);
+    public static readonly BindableProperty SelectedIndexProperty = BindableProperty.Create(nameof(SelectedIndex), typeof(int), typeof(LabeledPicker), -1, defaultBindingMode: BindingMode.TwoWay);
+    public static readonly BindableProperty SelectedItemProperty = BindableProperty.Create(nameof(SelectedItem), typeof(object), typeof(LabeledPicker), defaultBindingMode: BindingMode.TwoWay);
     public static readonly BindableProperty PlaceholderProperty = BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(LabeledPicker), propertyChanged: PlaceholderChanged);
     public static readonly BindableProperty TitleColorProperty = BindableProperty.Create(nameof(TitleColor), typeof(Color), typeof(LabeledPicker), Color.FromArgb("#808080"), propertyChanged: TitleColorChanged);
     public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(nameof(FontSize), typeof(double), typeof(LabeledPicker), LabelBase.DefaultFontSize, propertyChanged: FontSizeChanged);
+    public static readonly BindableProperty ItemDisplayBindingProperty = BindableProperty.Create(nameof(ItemDisplayBinding), typeof(BindingBase), typeof(LabeledPicker), propertyChanged: ItemDisplayBindingChanged);
 
     public int SelectedIndex
     {
@@ -63,21 +64,17 @@ public partial class LabeledPicker
 
     public BindingBase ItemDisplayBinding
     {
-        get => Element?.ItemDisplayBinding!;
-        set => Element.ItemDisplayBinding = value;
+        get => (BindingBase)GetValue(ItemDisplayBindingProperty);
+        set => SetValue(ItemDisplayBindingProperty, value);
     }
 
-    private static void ItemSourceChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledPicker)bindable).UpdateItemSourceView();
-    private static void SelectedIndexChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledPicker)bindable).UpdateSelectedIndex();
-    private static void SelectedItemChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledPicker)bindable).UpdateSelectedItem();
     private static void PlaceholderChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledPicker)bindable).UpdatePlaceholder();
     private static void TitleColorChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledPicker)bindable).UpdateTitleColorView();
     private static void FontSizeChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledPicker)bindable).UpdateFontSizeView();
-    
-    private void UpdateItemSourceView() => Element.ItemsSource = ItemsSource;
-    private void UpdateSelectedIndex() => Element.SelectedIndex = SelectedIndex;
-    private void UpdateSelectedItem() => Element.SelectedItem = SelectedItem;
+    private static void ItemDisplayBindingChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledPicker)bindable).UpdateItemDisplayBinding();
+
     private void UpdatePlaceholder() => Element.Title = Placeholder;
     private void UpdateFontSizeView() => Element.FontSize = FontSize;
     private void UpdateTitleColorView() => Element.TitleColor = TitleColor;
+    private void UpdateItemDisplayBinding() => Element.ItemDisplayBinding = ItemDisplayBinding;
 }

NiceEntry/LabeledDatePicker.xaml.cs
@@ -13,7 +13,7 @@ public partial class LabeledDatePicker
         UpdateFontSizeView();
     }
     
-    public static readonly BindableProperty DateProperty = BindableProperty.Create(nameof(Date), typeof(DateTime), typeof(LabeledDatePicker), propertyChanged: DateChanged, defaultBindingMode: BindingMode.TwoWay, defaultValueCreator: static _ => DateTime.Today);
+    public static readonly BindableProperty DateProperty = BindableProperty.Create(nameof(Date), typeof(DateTime), typeof(LabeledDatePicker), defaultBindingMode: BindingMode.TwoWay, defaultValueCreator: static _ => DateTime.Today);
     public static readonly BindableProperty MinimumDateProperty = BindableProperty.Create(nameof(MinimumDate), typeof(DateTime), typeof(LabeledDatePicker), DateTime.MinValue, propertyChanged: MinimumDateChanged);
     public static readonly BindableProperty MaximumDateProperty = BindableProperty.Create(nameof(MaximumDate), typeof(DateTime), typeof(LabeledDatePicker), DateTime.MaxValue, propertyChanged: MaximumDateChanged);
     public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(nameof(FontSize), typeof(double), typeof(LabeledDatePicker), LabelBase.DefaultFontSize, propertyChanged: FontSizeChanged);
@@ -42,12 +42,10 @@ public partial class LabeledDatePicker
         set => SetValue(FontSizeProperty, value);
     }
     
-    private static void DateChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledDatePicker)bindable).UpdateDateView();
     private static void MinimumDateChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledDatePicker)bindable).UpdateMinimumDateView();
     private static void MaximumDateChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledDatePicker)bindable).UpdateMaximumDateView();
     private static void FontSizeChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledDatePicker)bindable).UpdateFontSizeView();
     
-    private void UpdateDateView() => Element.Date = Date;
     private void UpdateMinimumDateView() => Element.MinimumDate = MinimumDate;
     private void UpdateMaximumDateView() => Element.MaximumDate = MaximumDate;
     private void UpdateFontSizeView() => Element.FontSize = FontSize;

NiceEntry/LabeledEntry.xaml.cs
@@ -12,13 +12,22 @@ public partial class LabeledEntry
         Element.SetBinding(Entry.TextProperty, nameof(Text), BindingMode.TwoWay);
         Element.BindingContext = this;
 
-        Element.Focused += OnElementFocused;
+        // Attach/detach the Focused handler with the control's load lifecycle so the
+        // inner Element doesn't keep the subscription (and a back-reference) alive
+        // across recycling. Detach-before-attach guards against duplicate handlers
+        // if Loaded fires more than once.
+        Loaded += (_, _) =>
+        {
+            Element.Focused -= OnElementFocused;
+            Element.Focused += OnElementFocused;
+        };
+        Unloaded += (_, _) => Element.Focused -= OnElementFocused;
 
         UpdateFontSizeView();
         UpdatePlaceholderColorView();
     }
     
-    public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(LabeledEntry), propertyChanged: TextChanged, defaultBindingMode: BindingMode.TwoWay);
+    public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(LabeledEntry), defaultBindingMode: BindingMode.TwoWay);
     public static readonly BindableProperty PlaceholderProperty = BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(LabeledEntry), propertyChanged: PlaceholderChanged);
     public static readonly BindableProperty PlaceholderColorProperty = BindableProperty.Create(nameof(PlaceholderColor), typeof(Color), typeof(LabeledEntry), Color.FromArgb("#808080"), propertyChanged: PlaceholderColorChanged);
     public static readonly BindableProperty MaxLengthProperty = BindableProperty.Create(nameof(MaxLength), typeof(int), typeof(LabeledEntry), int.MaxValue, propertyChanged: MaxLengthChanged);
@@ -111,7 +120,6 @@ public partial class LabeledEntry
         Element.SelectionLength = Element.Text?.Length ?? 0;
     }
 
-    private static void TextChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledEntry)bindable).UpdateTextView();
     private static void PlaceholderChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledEntry)bindable).UpdatePlaceholderView();
     private static void PlaceholderColorChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledEntry)bindable).UpdatePlaceholderColorView();
     private static void KeyboardChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledEntry)bindable).UpdateKeyboardView();
@@ -123,7 +131,6 @@ public partial class LabeledEntry
     private static void HorizontalTextAlignmentChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledEntry)bindable).UpdateHorizontalTextAlignmentView();
     private static void FontSizeChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledEntry)bindable).UpdateFontSizeView();
     
-    private void UpdateTextView() => Element.Text = Text;
     private void UpdatePlaceholderView() => Element.Placeholder = Placeholder;
     private void UpdatePlaceholderColorView() => Element.PlaceholderColor = PlaceholderColor;
     private void UpdateKeyboardView() => Element.Keyboard = Keyboard;

NiceEntry/LabeledTimePicker.xaml.cs
@@ -13,7 +13,7 @@ public partial class LabeledTimePicker
         UpdateFontSizeView();
     }
     
-    public static readonly BindableProperty TimeProperty = BindableProperty.Create(nameof(Time), typeof(TimeSpan), typeof(LabeledTimePicker), propertyChanged: TimeChanged, defaultBindingMode: BindingMode.TwoWay);
+    public static readonly BindableProperty TimeProperty = BindableProperty.Create(nameof(Time), typeof(TimeSpan), typeof(LabeledTimePicker), defaultBindingMode: BindingMode.TwoWay);
     public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(nameof(FontSize), typeof(double), typeof(LabeledTimePicker), LabelBase.DefaultFontSize, propertyChanged: FontSizeChanged);
     
     public TimeSpan Time
@@ -28,9 +28,6 @@ public partial class LabeledTimePicker
         set => SetValue(FontSizeProperty, value);
     }
     
-    private static void TimeChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledTimePicker)bindable).UpdateTimeView();
-    
-    private void UpdateTimeView() => Element.Time = Time;
     private static void FontSizeChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledTimePicker)bindable).UpdateFontSizeView();
     private void UpdateFontSizeView() => Element.FontSize = FontSize;
 }

commit f2a87574b2f905722dc4c933c583a193127677a3

fix: make ItemDisplayBinding a plain CLR property again (#37)
As a BindableProperty, XamlC compiles ItemDisplayBinding="{Binding X}" in
consumer XAML into SetBinding against the page's BindingContext instead of
assigning the binding object itself. The path never resolves on the view
model, the inner Picker's ItemDisplayBinding stays null, and rows fall back
to ToString() (the item's type name).

MAUI's own Picker.ItemDisplayBinding is deliberately a plain CLR property
for exactly this reason. This reverts the regression introduced in 3eb2653
(which closed #5) and restores the approach from 165630c.

Claude-Session: https://claude.ai/code/session_01WitcHAJWfFmjKdSHWXgHRv

NiceEntry/LabeledPicker.xaml.cs
@@ -24,7 +24,6 @@ public partial class LabeledPicker
     public static readonly BindableProperty PlaceholderProperty = BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(LabeledPicker), propertyChanged: PlaceholderChanged);
     public static readonly BindableProperty TitleColorProperty = BindableProperty.Create(nameof(TitleColor), typeof(Color), typeof(LabeledPicker), Color.FromArgb("#808080"), propertyChanged: TitleColorChanged);
     public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(nameof(FontSize), typeof(double), typeof(LabeledPicker), LabelBase.DefaultFontSize, propertyChanged: FontSizeChanged);
-    public static readonly BindableProperty ItemDisplayBindingProperty = BindableProperty.Create(nameof(ItemDisplayBinding), typeof(BindingBase), typeof(LabeledPicker), propertyChanged: ItemDisplayBindingChanged);
 
     public int SelectedIndex
     {
@@ -62,19 +61,20 @@ public partial class LabeledPicker
         set => SetValue(FontSizeProperty, value);
     }
 
+    // Deliberately a plain CLR property, mirroring MAUI's Picker.ItemDisplayBinding: as a
+    // BindableProperty, XAML applies "{Binding X}" against the BindingContext instead of
+    // assigning the binding itself, so the inner picker never receives it (#37).
     public BindingBase ItemDisplayBinding
     {
-        get => (BindingBase)GetValue(ItemDisplayBindingProperty);
-        set => SetValue(ItemDisplayBindingProperty, value);
+        get => Element.ItemDisplayBinding;
+        set => Element.ItemDisplayBinding = value;
     }
 
     private static void PlaceholderChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledPicker)bindable).UpdatePlaceholder();
     private static void TitleColorChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledPicker)bindable).UpdateTitleColorView();
     private static void FontSizeChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledPicker)bindable).UpdateFontSizeView();
-    private static void ItemDisplayBindingChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledPicker)bindable).UpdateItemDisplayBinding();
 
     private void UpdatePlaceholder() => Element.Title = Placeholder;
     private void UpdateFontSizeView() => Element.FontSize = FontSize;
     private void UpdateTitleColorView() => Element.TitleColor = TitleColor;
-    private void UpdateItemDisplayBinding() => Element.ItemDisplayBinding = ItemDisplayBinding;
 }