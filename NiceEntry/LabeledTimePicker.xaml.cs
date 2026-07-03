0909e556c8f56f4cb5c10252ffab51084a2066b4
Rickard Nord <rickard.nord@chargenode.eu>
2026-07-03 17:30:57 +0200

    feat: add ShowClearButton to LabeledTimePicker

--- NiceEntry/LabeledTimePicker.xaml.cs
+++ NiceEntry/LabeledTimePicker.xaml.cs
@@ -1,21 +1,34 @@
-namespace NiceEntry;
+﻿namespace NiceEntry;
 
 public partial class LabeledTimePicker
 {
     public LabeledTimePicker()
     {
         InitializeComponent();
-        
+
         Element.SetVisualElementBinding();
         Element.SetBinding(TimePicker.TimeProperty, nameof(Time), BindingMode.TwoWay);
+        // LabelBase sätter semantikbeskrivningen på View, som numera är Grid-wrappern —
+        // vidarebefordra etiketten till själva pickern så skärmläsare annonserar den där.
+        Element.SetBinding(SemanticProperties.DescriptionProperty, nameof(Label));
         Element.BindingContext = this;
 
+        // SetVisualElementBinding kopplar IsEnabled till Element, inte till ✕:et —
+        // lyssna själv så clear-knappen döljs när kontrollen disablas.
+        PropertyChanged += (_, e) =>
+        {
+            if (e.PropertyName == nameof(IsEnabled))
+                UpdateClearButtonView();
+        };
+
         UpdateFontSizeView();
+        UpdateClearButtonView();
     }
-    
-    public static readonly BindableProperty TimeProperty = BindableProperty.Create(nameof(Time), typeof(TimeSpan?), typeof(LabeledTimePicker), defaultBindingMode: BindingMode.TwoWay);
+
+    public static readonly BindableProperty TimeProperty = BindableProperty.Create(nameof(Time), typeof(TimeSpan?), typeof(LabeledTimePicker), defaultBindingMode: BindingMode.TwoWay, propertyChanged: TimeChanged);
     public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(nameof(FontSize), typeof(double), typeof(LabeledTimePicker), LabelBase.DefaultFontSize, propertyChanged: FontSizeChanged);
-    
+    public static readonly BindableProperty ShowClearButtonProperty = BindableProperty.Create(nameof(ShowClearButton), typeof(bool), typeof(LabeledTimePicker), false, propertyChanged: ShowClearButtonChanged);
+
     public TimeSpan? Time
     {
         get => (TimeSpan?)GetValue(TimeProperty);
@@ -27,7 +40,19 @@ public partial class LabeledTimePicker
         get => (double)GetValue(FontSizeProperty);
         set => SetValue(FontSizeProperty, value);
     }
-    
+
+    public bool ShowClearButton
+    {
+        get => (bool)GetValue(ShowClearButtonProperty);
+        set => SetValue(ShowClearButtonProperty, value);
+    }
+
+    private static void TimeChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledTimePicker)bindable).UpdateClearButtonView();
     private static void FontSizeChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledTimePicker)bindable).UpdateFontSizeView();
+    private static void ShowClearButtonChanged(BindableObject bindable, object oldValue, object newValue) => ((LabeledTimePicker)bindable).UpdateClearButtonView();
+
     private void UpdateFontSizeView() => Element.FontSize = FontSize;
+    private void UpdateClearButtonView() => ClearButton.IsVisible = ShowClearButton && Time is not null && IsEnabled;
+
+    private void OnClearTapped(object? sender, TappedEventArgs e) => Time = null;
 }
